c546150f159881cc46ba2fb10404d7ef6b38db29
swatijadhav <swati.jadhav@kiprosh.com>
2015-07-13 18:45:42 +0530

    intial config settings

---
+++ app/adapters/application.js
@@ -0,0 +1,23 @@
+import DS from 'ember-data';
+// import Ember from 'ember';
+import config from '../config/environment';
+
+export default DS.ActiveModelAdapter.extend({
+  namespace: config.APP.NAMESPACE,
+  host:      config.APP.API_HOST_URL,
+  headers: function() {
+    return {
+      "Accept-Language":        "en",
+      "X-GOODCITY-APP-NAME":    config.APP.NAME,
+      "X-GOODCITY-APP-VERSION": config.APP.VERSION,
+      "X-GOODCITY-APP-SHA":     config.APP.SHA
+    };
+  }.property(),
+
+  // without this, error is wrapped like this {__reason_with_error_thrown__:jqXHR,message:"",stack:""}
+  // it does add a stacktrace that would otherwise be missing but only relates to adapter
+  // instead of calling code so not that useful
+  ajaxError: function(jqXHR) {
+    return this._super(jqXHR);
+  }
+});
--- config/environment.js
+++ config/environment.js
@@ -13,9 +13,25 @@ module.exports = function(environment) {
       }
     },
 
+    contentSecurityPolicy: {
+      "img-src": "'self' data: https://res.cloudinary.com",
+      "style-src": "'self' 'unsafe-inline' https://maxcdn.bootstrapcdn.com",
+      "font-src": "'self' data: https://maxcdn.bootstrapcdn.com",
+      "object-src": "'self'"
+    },
+
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
+      // Here you can pass flags/options to your application instance
+      // when it is created
+      NAME: 'browse.goodcity',
+      // RESTAdapter Settings
+      NAMESPACE: 'api/v1',
+
+      PRELOAD_TYPES: [],
+      SHA: process.env.APP_SHA || "00000000",
+      VERSION: "1.0.0"
     }
   };
 
@@ -25,23 +41,37 @@ module.exports = function(environment) {
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     // ENV.APP.LOG_VIEW_LOOKUPS = true;
+
+    // RESTAdapter Settings
+    ENV.APP.API_HOST_URL = 'http://localhost:3000';
+
+    ENV.contentSecurityPolicy["connect-src"] = [
+      'http://localhost:4202',
+      'http://localhost:3000',
+      'https://api.cloudinary.com'
+    ].join(' ');
   }
 
   if (environment === 'test') {
     // Testem prefers this...
     ENV.baseURL = '/';
-    ENV.locationType = 'none';
+    ENV.locationType = 'auto';
 
     // keep test console output quieter
     ENV.APP.LOG_ACTIVE_GENERATION = false;
     ENV.APP.LOG_VIEW_LOOKUPS = false;
 
     ENV.APP.rootElement = '#ember-testing';
+
+    // RESTAdapter Settings
+    ENV.APP.API_HOST_URL = 'http://localhost:4202';
   }
 
   if (environment === 'production') {
 
   }
+
+  ENV.APP.SERVER_PATH  = ENV.APP.API_HOST_URL + '/' + ENV.APP.NAMESPACE;
 
   return ENV;
 };
