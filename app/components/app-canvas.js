0fa46ceb5b200cfcf376a4014c5840b08ec444c3
swatijadhav <swati.jadhav@kiprosh.com>
2015-07-21 08:30:49 +0530

    use didInsertElement block for foundation intialization

--- app/components/app-canvas.js
+++ app/components/app-canvas.js
@@ -2,11 +2,9 @@ import Ember from "ember";
 
 export default Ember.Component.extend({
 
-  didInitAttrs() {
-    Ember.run.schedule("afterRender", function() {
-      Ember.$(document).foundation({
-        offcanvas: { close_on_click: true }
-      });
+  didInsertElement() {
+    Ember.$(document).foundation({
+      offcanvas: { close_on_click: true }
     });
   },
 
