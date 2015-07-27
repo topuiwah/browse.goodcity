027400d68367adf7c71d7a916b977e26ccb56f94
Shivani <shivanibhanwal@gmail.com>
2015-07-27 23:42:07 +0530

    GCW-515 comment# Fixed font size for the sidebar Fixed sidebar styles for large and small screen

--- app/components/app-canvas.js
+++ app/components/app-canvas.js
@@ -1,6 +1,21 @@
 import Ember from "ember";
 
 export default Ember.Component.extend({
+  screenResized: function() {
+    return matchMedia(Foundation.media_queries.small).matches &&
+      !matchMedia(Foundation.media_queries.medium).matches;
+  },
+
+  observeScreen: function() {
+    if (!this.screenResized()){
+      $('.off-canvas-wrap').removeClass('move-right');
+    }
+  },
+
+  initComonent: function() {
+    var updateScreen = Ember.run.bind(this, this.observeScreen);
+    window.addEventListener("resize", updateScreen);
+  }.on("init"),
 
   didInsertElement() {
     Ember.$(document).foundation({
