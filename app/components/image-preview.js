a58e7db838ee0a62ba380fbf62281e3d883aa19d
swatijadhav <swati.jadhav@kiprosh.com>
2015-07-27 18:19:34 +0530

    display preview on image-click for mobile view

--- app/components/image-preview.js
+++ app/components/image-preview.js
@@ -2,15 +2,44 @@ import Ember from "ember";
 
 export default Ember.Component.extend({
 
-  didInsertElement() {
-    Ember.$("#lightGallery").lightGallery({
+  lightGalleryObj: null,
+
+  isSmallScreen: function(key, value){
+    return (arguments.length > 1) ? value : this.screenResized();
+  }.property(),
+
+  screenResized: function() {
+    return matchMedia(Foundation.media_queries.small).matches &&
+      !matchMedia(Foundation.media_queries.medium).matches;
+  },
+
+  observeScreen: function() {
+    this.set("isSmallScreen", this.screenResized());
+    this.initializeLightgallery();
+  },
+
+  initComonent: function() {
+    var updateScreen = Ember.run.bind(this, this.observeScreen);
+    window.addEventListener("resize", updateScreen);
+  }.on("init"),
+
+  initializeLightgallery: function(){
+    var gallery = this.get("lightGalleryObj");
+    if(gallery) { gallery.destroy(); }
+
+    var lightGalleryObj = Ember.$("#lightGallery").lightGallery({
       thumbnail: false,
       hideControlOnEnd: true,
       closable: false,
       counter: true,
       swipeThreshold : 50,
-      enableTouch : true,
+      enableTouch : true
     });
+    this.set('lightGalleryObj', lightGalleryObj);
+  },
+
+  didInsertElement() {
+    this.initializeLightgallery();
   },
 
 });
