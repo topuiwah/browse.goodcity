8927068a8cc025c89136244faeda2abdcf343338
Shivani <shivanibhanwal@gmail.com>
2015-07-29 23:10:13 +0530

    GCW-515 comment# Fixed sidebar styles now sidebar is Sticky and also off-canvas it also uses now same code for the large and small screen sidebar Styles are also consistent now for small and large sidebar font sizes fixed for the browse screen and sidebar. Wrote common scss for the font-size use and sidebar

--- app/components/app-canvas.js
+++ app/components/app-canvas.js
@@ -2,16 +2,35 @@ import Ember from "ember";
 import ObserveScreenResize from "./observe-screen-resize";
 
 export default ObserveScreenResize.extend({
+
   observeScreen: function() {
     if (!this.screenResized()){
-      Ember.$('.off-canvas-wrap').removeClass('move-right');
+      Ember.$('.off-canvas-wrap').addClass('move-right');
+      Ember.$('.left-off-canvas-toggle').hide();
+      this.OtherScreenOffCanvas();
     }
+    else
+    {
+      Ember.$('.off-canvas-wrap').removeClass('move-right');
+      Ember.$('.left-off-canvas-toggle').show();
+      this.smallScreenOffCanvas();
+    }
+  },
+  smallScreenOffCanvas: function(){
+    Ember.$(document).foundation({ offcanvas: { close_on_click: true } });
+  },
+  OtherScreenOffCanvas: function(){
+    Ember.$(document).foundation({offcanvas: { close_on_click: false }});
   },
 
   didInsertElement() {
-    Ember.$(document).foundation({
-      offcanvas: { close_on_click: true }
-    });
+    if (!this.screenResized()){
+      this.OtherScreenOffCanvas();
+    }
+    else{
+      this.smallScreenOffCanvas();
+      Ember.$('.off-canvas-wrap').removeClass('move-right');
+    }
   },
 
 });
