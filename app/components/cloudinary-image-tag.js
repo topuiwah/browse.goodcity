33840f693d25a4626e07b4f909fba1155e05eabc
swatijadhav <swati.jadhav@kiprosh.com>
2015-08-04 20:27:59 +0530

    remove loading for image if its already cached

--- app/components/cloudinary-image-tag.js
+++ app/components/cloudinary-image-tag.js
@@ -6,16 +6,29 @@ export default Ember.Component.extend({
   }.property(),
 
   changedSrc: function() {
-    this.set("loading", true);
+    if(!this.is_cached(this.get("src"))) {
+      this.set("loading", true);
+    }
   }.observes("src"),
 
   onLoad: function() {
     this.set("loading", false);
   },
 
+  is_cached: function(src) {
+    var image = new Image();
+    image.src = src;
+    return image.complete;
+  },
+
   didInsertElement() {
     var updateScreen = Ember.run.bind(this, this.onLoad);
     var image_tag = this.element.getElementsByClassName('cl-item-image')[0];
     image_tag.addEventListener("load", updateScreen);
+  },
+
+  willDestroyElement() {
+    var image_tag = this.element.getElementsByClassName('cl-item-image')[0];
+    image_tag.removeEventListener("load");
   },
 });
