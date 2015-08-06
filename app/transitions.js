a4e8f605b295bb10c88a4fed3ece69ac6984a86b
swatijadhav <swati.jadhav@kiprosh.com>
2015-08-06 13:52:05 +0530

    left-right animations for browse, category and item route

--- app/transitions.js
+++ app/transitions.js
@@ -1,6 +1,22 @@
 export default function(){
   this.transition(
-    this.use('toLeft'),
-    this.reverse('toRight')
+    this.fromRoute('browse'),
+    this.toRoute('package_category'),
+    this.use('toLeft', {duration: 800}),
+    this.reverse('toRight', {duration: 800})
+  );
+
+  this.transition(
+    this.fromRoute('package_category'),
+    this.toRoute('item'),
+    this.use('toLeft', {duration: 500}),
+    this.reverse('toRight', {duration: 500})
+  );
+
+  this.transition(
+    this.fromRoute('browse'),
+    this.toRoute('item'),
+    this.use('toLeft', {duration: 500}),
+    this.reverse('toRight', {duration: 500})
   );
 }
