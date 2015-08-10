ecac67062873f0344e6f8a21f0cea3e2c3f9a64d
swatijadhav <swati.jadhav@kiprosh.com>
2015-08-10 13:14:03 +0530

    Updated animation transitions: - When going from a list view to an item view, the item view should fade in. - When going from an item view to a list view, the item view should fade out. - When going from top-level browse into a category or subcategory, the content should slide up from below

--- app/transitions.js
+++ app/transitions.js
@@ -2,21 +2,21 @@ export default function(){
   this.transition(
     this.fromRoute('browse'),
     this.toRoute('package_category'),
-    this.use('toLeft', {duration: 800}),
-    this.reverse('toRight', {duration: 800})
+    this.use('toUp', {duration: 800}),
+    this.reverse('toDown', {duration: 800})
   );
 
   this.transition(
     this.fromRoute('package_category'),
     this.toRoute('item'),
-    this.use('toLeft', {duration: 500}),
-    this.reverse('toRight', {duration: 500})
+    this.use('fade', {duration: 500}),
+    this.reverse('fade', {duration: 500})
   );
 
   this.transition(
     this.fromRoute('browse'),
     this.toRoute('item'),
-    this.use('toLeft', {duration: 500}),
-    this.reverse('toRight', {duration: 500})
+    this.use('toUp', {duration: 800}),
+    this.reverse('toDown', {duration: 800})
   );
 }
