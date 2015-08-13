33c49cc55bdf54b2dc61c53f8695e381aa67842a
Shivani <shivanibhanwal@gmail.com>
2015-08-13 18:19:09 +0530

    GCW-543 comment# I have added chinese translation for the text. Please change it as reuqired for HK. Also there was missing translation changes for the ALL word so I have fixed that

--- app/locales/zh/translations.js
+++ app/locales/zh/translations.js
@@ -4,7 +4,7 @@ export default {
   "company.name": "Crossroads Foundation",
   "by": "By",
   "application": {
-    "loading": "Loading",
+    "loading": "載入中",
     "sidebar": {
       "browse": "瀏覽",
       "myorders": "我的訂單",
@@ -12,29 +12,30 @@ export default {
     }
   },
   "browse": {
-    "title": ""
+    "title": "瀏覽產品"
   },
 
   "category": {
-    "filter": "",
+    "filter": "過濾項",
+    "all": "所有",
     "sort": {
-      "title": "",
-      "newfirst": "",
-      "oldfirst": ""
+      "title": "排序方式",
+      "newfirst": "新的排前",
+      "oldfirst": "最舊的"
     }
   },
 
   "itemdetail": {
-    "view": "",
-    "previous": "",
-    "next": "",
-    "condition": "",
-    "quantity": "",
-    "size": "",
-    "categories": ""
+    "view": "查看項目",
+    "previous": "上一個商品",
+    "next": "下一個商品",
+    "condition": "條件",
+    "quantity": "數量",
+    "size": "尺寸",
+    "categories": "分類"
   },
   "page": {
-    "previous": "",
-    "next": ""
+    "previous": "前",
+    "next": "下一個"
   }
 };
