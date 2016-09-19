991c5eb99afdc58a60fc73016f1cf6c83545ca8b
swatijadhav <swati.jadhav@kiprosh.com>
2016-09-19 14:03:50 +0530

    cart toggle details view for small-screen

---
+++ app/services/screenresize.js
@@ -0,0 +1,22 @@
+import Ember from 'ember';
+import '../computed/local-storage';
+
+export default Ember.Service.extend({
+  isSmallScreen: false,
+
+  screenResized: function() {
+    return matchMedia(Foundation.media_queries.small).matches &&
+      !matchMedia(Foundation.media_queries.medium).matches;
+  },
+
+  observeScreen: function() {
+    this.set("isSmallScreen", this.screenResized());
+  },
+
+  initComonent: function() {
+    this.observeScreen();
+    var updateScreen = Ember.run.bind(this, this.observeScreen);
+    window.addEventListener("resize", updateScreen);
+  }.on("init"),
+
+});
