54f98fcdf968de4d6c5908f6c47ae635e16ed2ca
swatijadhav <swati.jadhav@kiprosh.com>
2016-09-21 20:44:44 +0530

    transport-details UI changes

--- app/controllers/order/transport_details.js
+++ app/controllers/order/transport_details.js
@@ -2,4 +2,14 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   order: Ember.computed.alias("model.order"),
+
+  selectedDate: null,
+  selectedTime: null,
+
+  timeSlots: Ember.computed(function(){
+    return [
+      {id: "1", name: "10:30AM-1PM"},
+      {id: "2", name: "2PM-4PM"},
+    ];
+  }),
 });
