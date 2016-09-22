1e303348714ecc8e0870fd9886c4afde2d3d35df
namrataukirde <namrata.ukirde@kiprosh.com>
2016-09-22 19:39:12 +0530

    GCW-1252-image-layout-changes-for-small-screen

--- app/models/item.js
+++ app/models/item.js
@@ -37,11 +37,15 @@ export default DS.Model.extend(cloudinaryImage, {
   }),
 
   otherImages: Ember.computed('images.[]', function(){
-    var images =  this.get("images").toArray();
-    return images.filter((image, index, self) => self.findIndex((t) => t.get('cloudinaryId') === image.get('cloudinaryId')) === index);
+    var images = this.get('images').filter((image, index, self) => self.findIndex((t) => t.get('cloudinaryId') === image.get('cloudinaryId')) === index);
+    return images.removeObject(this.get('favouriteImage'));
   }),
 
-  sortedImages: Ember.computed.alias('otherImages'),
+  sortedImages: Ember.computed('otherImages.[]', 'image', function(){
+    var images = this.get('otherImages').toArray();
+    images.unshift(this.get("favouriteImage"));
+    return images;
+  }),
 
   displayImage: Ember.computed('images.@each.favourite', function() {
     return this.get("favouriteImage") ||
