90ee9cd8a8aced859dfee6aaae56b753c128dbdd
namrataukirde <namrata.ukirde@kiprosh.com>
2016-09-30 16:15:40 +0530

    GCW-1277-update-translation-in-all-pages

--- app/locales/en/translations.js
+++ app/locales/en/translations.js
@@ -8,6 +8,15 @@ export default {
   "must_login": "You must login!",
   "okay": "Okay",
   "unexpected_error": "Something went wrong",
+  "show": "Show",
+  "date": "Date",
+  "time": "Time",
+  "contact": "Contact",
+  "draft": "draft",
+  "submitted": "submitted",
+  "processing": "processing",
+  "closed": "closed",
+  "cancelled": "cancelled",
 
   "application": {
     "loading": "Loading",
@@ -40,10 +49,12 @@ export default {
     "size": "Size",
     "categories": "Categories"
   },
+
   "page": {
     "previous": "Previous",
     "next": "Next"
   },
+
   "login": {
     "hk_only" : "Mobile phone (Hong Kong only)",
     "login" : "Login",
@@ -80,9 +91,77 @@ export default {
       "time": "Time",
       "am": "AM",
       "pm": "PM",
-      "vehicle": "Vehicle Type"
+      "vehicle": "Vehicle Type",
+      "delivery_district": "Delivery district"
     }
+  },
+
+  "my_orders": {
+    "my_orders": "My Orders",
+    "order_no": "Order No",
+    "order_state": "Order State",
+    "item_count": "Item Count"
+  },
+
+  "item": {
+    "request_item": "Request Item",
+    "remove_item": "Remove Item"
+  },
+
+  "cart_content": {
+    "your_request": "Your Request",
+    "continue_browsing": "Continue Browsing",
+    "submit_request": "Submit Request",
+    "remove": "Remove"
+  },
+
+  "order": {
+    "organisation": "Organisation",
+    "representative": "Representative",
+    "purpose": "Purpose(s)",
+    "login": "Log in",
+    "goodcity_order": "GoodCity Order",
+    "cart": "Cart",
+    "order_details": "Order details",
+    "transport_detail": "Transport details",
+    "confirm": "Confirm",
+    "tick_any": "Tick any that apply",
+    "select_purpose": "Please select purpose from list.",
+    "for_our_charity": "For our charity (facilities / programs etc.)",
+    "for_client": "For client / beneficiary (personal use)",
+    "for_charity_sale": "For charity sale, bartering (any form of trade)",
+    "return_to_cart": "Return to cart",
+    "submit_this_order": "Submit this order",
+    "limited_crossroads_opening_days_and_time": "Limited to Crossroad's opening days/times.",
+    "continue_to_confirm_details": "Continue to confirm details",
+    "continue_to_transport_details": "Continue to transport details",
+    "hired_van": "Hired van delivery(GoGoVan)",
+    "self_vehicle": "Self vehicle",
+    "transport_details": {
+      "transport_method": "Transport method",
+      "collect_with_own_vehicle": "We will collect with our own vehicle(s)",
+      "send_item_with_driver": "We would like the items sent (recipient pays the driver)"
+    },
+    "vehicle": {
+      "collection_date_time": "Collection date / time"
+    },
+    "gogovan": {
+      "requirements": {
+        "speak_english": "Speak English",
+        "borrow": "Borrow trolley",
+        "carry": "Need Carry"
+      }
+    },
+    "cart_details": {
+      "cart_summary": "Show order summary"
+    }
+  },
+  'plan_transport': {
+    'first_come_first_serve': 'Goods are granted on "first come first served" basis.',
+    'reserved_text': 'Your requested item will be reserved on {{scheduledDate}}. On that day we will call you to confirm which of the requested items are available before finalizing the order.',
+    'info_text': 'If another person asks for an item you have requested and they arrange the transport before {{scheduledDate}}, they will get the item and you will not.',
+    'arrange_transport': 'To guarantee your order, arrange transport for {{todaysDate}}',
+    'reserve_your_items': '(we would then reserve your items today).',
+    'explanation_text': 'Each day we put aside items due to dispatch the following day. It is too costly and complicated for us to reserve items for longer. If we are unable to contact you to confirm or if you do not arrive on the {{scheduledDate}}, the items will be released back into stock and your order canceled.'
   }
-
-
 };
