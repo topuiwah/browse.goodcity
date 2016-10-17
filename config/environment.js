16380704ba4a653073b26dfeb640aeb6de29fd7f
namrataukirde <namrata.ukirde@kiprosh.com>
2016-10-17 17:45:24 +0530

    adding app icons

--- config/environment.js
+++ config/environment.js
@@ -5,7 +5,7 @@ module.exports = function(environment) {
     modulePrefix: 'browse',
     environment: environment,
     rootURL: '/',
-    locationType: 'auto',
+    defaultLocationType: 'auto',
     
     EmberENV: {
       FEATURES: {
