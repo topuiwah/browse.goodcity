3077541a3685659493ae89be5a9a22e60eabf0bd
Darshan-Chauhan <darshan.chauhan@kiprosh.com>
2016-11-22 16:01:09 +0530

    GCW-1343 Fixed: Cannot read property abort of null

--- app/routes/item.js
+++ app/routes/item.js
@@ -1,7 +1,35 @@
 import PublicRoute from './browse_pages';
+import Ember from 'ember';
 
-export default PublicRoute.extend({  model(params) {
-    return this.store.peekRecord('item', params["id"]);
+export default PublicRoute.extend({
+  messageBox: Ember.inject.service(),
+  isPublished: null,
+  transition: null,
+
+  beforeModel(transition) {
+    this._super(...arguments);
+    this.set('transition', transition);
+  },
+
+  model(params, transition) {
+    var item = this.store.peekRecord('item', params["id"]);
+    if(!item) {
+      this.set('isPublished', false);
+    } else {
+      this.set('isPublished', true);
+    }
+    this.set('transition', transition);
+    return item;
+  },
+
+  afterModel() {
+    if(this.get('isPublished') !== null && !this.get('isPublished')) {
+      this.get('transition').abort();
+      this.get('messageBox').alert('Sorry! This item is no longer available.',
+        () => {
+          this.transitionTo('/browse');
+        });
+    }
   },
 
   setupController(controller, model){
