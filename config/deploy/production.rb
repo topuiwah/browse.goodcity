2a12d513b0e3100c6cf711f4c8f6e1fdf8d6963f
Steve Kenworthy <steveyken@gmail.com>
2017-05-18 11:03:27 +0800

    Fixed deployment server addresses

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,3 @@
 server 'browse.goodcity.hk:62422', user: 'deployer', roles: %w{web}, primary: true
-server 'goodcity-production.cloudapp.net:62423', user: 'deployer', roles: %w{web}
+# server 'goodcity-production.cloudapp.net:62423', user: 'deployer', roles: %w{web}
 set :branch, :live
