a632f5b00fbb4b28c9416f3303a6fa8fa0b52dca
trojanh <rajan.tiwari@kiprosh.com>
2017-05-30 15:57:54 +0530

    GCW-1493-remove 'Sorry! This item is no longer available.' for Items

--- app/routes/item.js
+++ app/routes/item.js
@@ -3,34 +3,17 @@ import Ember from 'ember';
 
 export default PublicRoute.extend({
   messageBox: Ember.inject.service(),
-  isPublished: null,
   transition: null,
 
   beforeModel(transition) {
     this._super(...arguments);
     this.set('transition', transition);
-    this.set('isPublished', null);
   },
 
   model(params, transition) {
     var item = this.store.peekRecord('item', params["id"]);
-    if(!item || !item.get("quantity")) {
-      this.set('isPublished', false);
-    } else {
-      this.set('isPublished', true);
-    }
     this.set('transition', transition);
     return item;
-  },
-
-  afterModel() {
-    if(this.get('isPublished') !== null && !this.get('isPublished')) {
-      this.get('transition').abort();
-      this.get('messageBox').alert('Sorry! This item is no longer available.',
-        () => {
-          this.transitionTo('/browse');
-        });
-    }
   },
 
   setupController(controller, model){
