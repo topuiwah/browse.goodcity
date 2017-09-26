334df1185882b8fa27da7a5babe1384261df2fff
Darshan-Chauhan <darshan.chauhan@kiprosh.com>
2017-09-26 11:01:17 +0530

    CI build fixed

--- tests/integration/component/auto-resize-textarea-test.js
+++ tests/integration/component/auto-resize-textarea-test.js
@@ -29,8 +29,8 @@ test('is of textarea type', function(assert) {
   assert.equal($('#description')[0].type, "textarea");
 });
 
-test('initial height is 70px', function(assert) {
+test('initial height is 66px', function(assert) {
   assert.expect(1);
-  assert.equal($('#description').css('height'), "70px");
+  assert.equal($('#description').css('height'), "66px");
 });
 
