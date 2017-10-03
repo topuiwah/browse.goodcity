2135352fdac06d19a5d2b7e3074035f4238b1c32
namrataukirde <namrata.ukirde@kiprosh.com>
2017-10-03 21:22:41 +0530

    package-type-model-test-case

--- tests/unit/models/package-type-test.js
+++ tests/unit/models/package-type-test.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 import { test, moduleForModel } from 'ember-qunit';
 
 moduleForModel('package-type', {
-  needs: ['model:item', 'model:package']
+  needs: ['model:item', 'model:package', 'model:image', 'model:donor-condition']
 });
 
 test('item relationship', function(assert) {
@@ -23,4 +23,49 @@ test('Valid ember-data Model', function(assert) {
   });
 
   assert.equal(record.get('name'), "Test");
+});
+
+test('_packages: Returns all packages', function(assert){
+  var package1, package2, store, model;
+
+  store = this.store();
+  model = this.subject();
+
+  Ember.run(function(){
+    package1 = store.createRecord('package', { id: 1, quantity: 1 });
+    package2 = store.createRecord('package', { id: 2, quantity: 1 });
+  });
+
+  assert.equal(model.get('_packages.length'), 2);
+});
+
+test('getItemPackageList: it returns single packages with no siblings', function(assert){
+  var package1, store, model;
+
+  store = this.store();
+  model = this.subject();
+
+  Ember.run(function(){
+    package1 = store.createRecord('package', { id: 1, quantity: 1, isAvailable: true, hasSiblingPackages: false });
+    model.get('packages').pushObjects([package1]);
+  });
+
+  assert.equal(model.get('getItemPackageList.length'), 1);
+  assert.equal(Ember.compare(model.get('getItemPackageList'), [package1]), 0);
+});
+
+test('getItemPackageList: list of associated items if package has sibling', function(assert){
+  var package1, item, store, model;
+
+  store = this.store();
+  model = this.subject();
+
+  Ember.run(function(){
+    item = store.createRecord('item', { id: 1, saleable: true });
+    package1 = store.createRecord('package', { id: 1, quantity: 1, isAvailable: true, hasSiblingPackages: true, item: item });
+    model.get('packages').pushObjects([package1]);
+  });
+
+  assert.equal(model.get('getItemPackageList.length'), 1);
+  assert.equal(Ember.compare(model.get('getItemPackageList'), [item]), 0);
 });
