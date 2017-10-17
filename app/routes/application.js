7d325d91f40c562b9bcabc3d665382f3fd6dfc94
Darshan-Chauhan <darshan.chauhan@kiprosh.com>
2017-10-17 10:38:44 +0530

    Window reloaded on deleting authtoken

--- app/routes/application.js
+++ app/routes/application.js
@@ -20,7 +20,7 @@ export default Ember.Route.extend(preloadDataMixin, {
       if(!window.localStorage.getItem('authToken') && !object.get('isMustLoginAlreadyShown') && !(currentPath.includes("login") || currentPath.includes("authenticate") || currentPath.includes("#/browse") || currentPath.includes("#/category") || currentPath.includes("#/package") || currentPath.includes("#/item"))) {
         object.set('isMustLoginAlreadyShown', true);
         object.get('messageBox').alert(object.get("i18n").t('must_login'), () => {
-          object.transitionTo("login");
+          window.location.reload();
         });
       }
     };
