4fe0b21659945e11e5ff23b85867d35947351aff
Darshan Chauhan <darshan.chauhan@kiprosh.com>
2018-03-05 11:56:51 +0530

    Revert "GCW-1825 Cannot read property get of null"

--- app/controllers/order_details.js
+++ app/controllers/order_details.js
@@ -47,10 +47,8 @@ export default applicationController.extend({
       var loadingView = getOwner(this).lookup('component:loading').append();
 
       this.get("cart.cartItems").forEach(record => {
-        if (record) {
-          var ids = record.get("isItem") ? record.get("packages").map(pkg => pkg.get("id")) : [record.get("id")];
-          package_ids = package_ids.concat(ids);
-        }
+        var ids = record.get("isItem") ? record.get("packages").map(pkg => pkg.get("id")) : [record.get("id")];
+        package_ids = package_ids.concat(ids);
       });
 
       var orderParams = {
