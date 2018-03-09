927e54a8cbffc1121c4e9935eec7cc68e8eb15ae
Darshan-Chauhan <darshan.chauhan@kiprosh.com>
2018-03-09 10:35:18 +0530

    CI build fixed

--- app/models/address.js
+++ app/models/address.js
@@ -1,12 +1,21 @@
 import Model from 'ember-data/model';
 import attr from 'ember-data/attr';
 import { belongsTo } from 'ember-data/relationships';
+import Ember from 'ember';
 
 export default Model.extend({
+  flat:        attr('string'),
+  building:    attr('string'),
+  street:      attr('string'),
+  addressType: attr('string'),
 
   district: belongsTo('district', { async: false }),
 
   addressableType: attr('string'),
-  addressable: belongsTo('addressable', { polymorphic: true, async: false })
+  addressable: belongsTo('addressable', { polymorphic: true, async: false }),
+
+  fullAddress: Ember.computed('flat', 'building', 'street', function() {
+    return `${this.get('flat')} ${this.get('building')} ${this.get('street')}`;
+  })
 
 });
