362a712df91a8a327ab59db91a2928bf8fb363ae
Darshan-Chauhan <darshan.chauhan@kiprosh.com>
2018-03-09 11:50:20 +0530

    OrderTranspot model test cases added

--- app/models/order_transport.js
+++ app/models/order_transport.js
@@ -18,6 +18,21 @@ export default Model.extend({
 
   needEnglish: attr("boolean"),
   needCart: attr("boolean"),
-  needCarry: attr("boolean")
+  needCarry: attr("boolean"),
+
+  scheduledDate: Ember.computed('scheduledAt', function() {
+    return moment(this.get('scheduledAt')).format("D MMMM YYYY");
+  }),
+
+  type: Ember.computed('transportType', function(){
+    var type = this.get('transportType');
+    if(type === "ggv"){
+      return type.toUpperCase();
+    } else if(type === "self"){
+      return type.charAt(0).toUpperCase() + type.slice(1);
+    } else {
+      return "";
+    }
+  })
 
 });
