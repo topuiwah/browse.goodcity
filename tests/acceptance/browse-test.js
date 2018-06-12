ec3a04df7b072e8f05f9b3845091a7fd48d91c6a
AbulAsar <abul.a@kiprosh.com>
2018-06-12 18:15:41 +0530

    test case for unload of order and orders_package on logout added

--- tests/acceptance/browse-test.js
+++ tests/acceptance/browse-test.js
@@ -2,8 +2,10 @@ import Ember from 'ember';
 import { module, test } from 'qunit';
 import startApp from '../helpers/start-app';
 import {make} from 'ember-data-factory-guy';
+import { mockFindAll } from 'ember-data-factory-guy';
+import FactoryGuy from 'ember-data-factory-guy';
 
-var App, pkgCategory1, pkgCategory2, subcategory1, pkgType, category_title, subcategory_title, empty_category_title, order, orders_packages;
+var App, pkgCategory1, pkgCategory2, subcategory1, pkgType, category_title, subcategory_title, empty_category_title, order, orders_package, gogo_van;
 
 module('Acceptance | Browse Page', {
   beforeEach: function() {
@@ -14,12 +16,20 @@ module('Acceptance | Browse Page', {
     pkgCategory2 = make("parent_package_category");
     order = make("order");
     orders_package = make("orders_package");
+    gogo_van = make("gogovan_transport");
+
+    mockFindAll("gogovan_transport").returns({json: {gogovan_transports: [gogo_van.toJSON({includeId: true})]}});
+
+    var data = {"user_profile": [{"id": 2,"first_name": "David", "last_name": "Dara51", "mobile": "61111111", "user_role_ids": [1]}], "users": [{"id": 2,"first_name": "David", "last_name": "Dara51", "mobile": "61111111"}], "roles": [{"id": 4, "name": "Supervisor"}], "user_roles": [{"id": 1, "user_id": 2, "role_id": 4}]};
+    $.mockjax({url:"/api/v1/auth/current_user_profil*",
+      responseText: data });
 
     mockFindAll("order").returns({
       json:
         {orders: [order.toJSON({includeId: true})],
         orders_packages: [orders_package.toJSON({includeId: true})]}
       });
+
     category_title = pkgCategory1.get('name') + " ("+ pkgCategory1.get("items.length") +")";
     subcategory_title = subcategory1.get('name') + " ("+ subcategory1.get("items.length") +")";
     empty_category_title = pkgCategory2.get('name') + " ("+ pkgCategory2.get("items.length") +")";
@@ -57,9 +67,14 @@ test("should list main-category without subcategories if has no items", function
 });
 
 test("clear orders and orders_packages from ember data on logout", function(assert){
-  visit("/");
-  click(".left-off-canvas-menu li:nth-child(4)");
-  andThen(function(){
-    assert.equal(currentURL(), "/browse");
+  var store = FactoryGuy.store;
+  visit("/").then(function() {
+    assert.equal(currentURL(), '/browse');
+    click(".left-off-canvas-menu li:nth-child(4) a");
+    andThen(function(){
+      assert.equal(currentURL(), "/browse");
+      assert.equal(store.peekAll("order").get("length"), 0);
+      assert.equal(store.peekAll("orders_package").get("length"), 0);
+    });
   });
 });
